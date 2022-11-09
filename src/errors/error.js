fced13f1ffe836b34ff4d25756c6064ca80df3c9
joyce <joyce.mbt@gmail.com>
2022-11-09 10:17:10 -0300

    criei o erro para fazer a validação do login

--- src/errors/error.js
+++ src/errors/error.js
@@ -4,5 +4,8 @@ export const errorMessage = {
             "400": "Informe todos os dados",
             "403": "Esse e-mail já está cadastrado",
         },
+        "login": {
+            "400": "E-mail ou senha inválido",
+        }
     }]
 }
