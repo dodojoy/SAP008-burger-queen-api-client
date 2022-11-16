23d7f8a313f1e4c7151fd2c2cc2ad11ac31d8104
joyce <joyce.mbt@gmail.com>
2022-11-16 16:36:30 -0300

    acessei os produtos da api

--- src/pages/salon/salon.js
+++ src/pages/salon/salon.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { menu } from "../../contexts/api";
 
 export const Salon = () => {
+  menu()
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => error);
   return (
     <div>Salon</div>
   );
