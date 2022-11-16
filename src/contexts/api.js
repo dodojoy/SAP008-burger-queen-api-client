621eb392df0d0dd49e5c7d7835fceb6729e991de
joyce <joyce.mbt@gmail.com>
2022-11-16 16:35:18 -0300

    mudei o nome do arquivo e criei o fetch para acessar os produtos da api

---
+++ src/contexts/api.js
@@ -0,0 +1,48 @@
+export const createUser = (name, email, password, role) => {
+  console.log(name, email, password, role);
+  return fetch('https://lab-api-bq.onrender.com/users', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      role: role,
+      restaurant: 'Burguer Queen',
+    }),
+  });
+};
+
+export const login = (email, password) => {
+  return fetch('https://lab-api-bq.onrender.com/auth', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
+};
+
+export const saveToken = (token) => {
+  localStorage.setItem('token', token);
+}
+
+const token = () => localStorage.getItem('token')
+
+
+export const menu = () => {
+  return fetch('https://lab-api-bq.onrender.com/products', {
+    method: 'GET',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': token(),
+    },
+  })
+};
+
+// return fetch('https://lab-api-bq.up.onrender.app/products', {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json',
+//       'Authorization': token },
+//     }),
