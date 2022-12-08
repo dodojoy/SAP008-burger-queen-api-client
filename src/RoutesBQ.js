0a90079e9f6255453ace935dad4c327fc4d0012a
joyce <joyce.mbt@gmail.com>
2022-12-08 15:23:17 -0300

    substituí browserrouter por hashrouter

--- src/RoutesBQ.js
+++ src/RoutesBQ.js
@@ -4,13 +4,13 @@ import { Salon } from './pages/salon/salon';
 import { Kitchen } from './pages/kitchen/kitchen';
 import { Fragment } from 'react';
 import {
-  BrowserRouter,
-  Routes, Route
+  HashRouter,
+  Routes, Route,
 } from 'react-router-dom';
 
 export const RoutesBQ = () => {
  return (
-  <BrowserRouter>
+  <HashRouter>
     <Fragment>
       <Routes>
         <Route path='/' element={<Login />} />
@@ -20,6 +20,6 @@ export const RoutesBQ = () => {
         <Route exact path='/kitchen' element={<Kitchen />} />
       </Routes>
     </Fragment>
-  </BrowserRouter>
+  </HashRouter>
  );
 }
