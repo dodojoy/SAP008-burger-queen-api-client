6174e24b5f59f41a7b4bf8b15ac7e8ae3e693150
joyce <joyce.mbt@gmail.com>
2022-12-15 16:48:27 -0300

    adicionei uma classe no btn para realizar a estilização

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 export function FooterAuth({ text1, text2, onClick }) {
 	return (
-		<p className="footer-auth">{text1} <button onClick={onClick}>{text2}</button></p>
+		<p className="footer-auth">{text1} <button className="navigate-btn" onClick={onClick}>{text2}</button></p>
 	);
 }
