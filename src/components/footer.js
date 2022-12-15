01c3dc2d2d0d1e2394dcbcf1afae1f9233afa3f1
joyce <joyce.mbt@gmail.com>
2022-12-15 16:25:38 -0300

    mudei a tag a para um button, onde vai gerar um evento para mudar de página

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
-export function FooterAuth({ text1, text2, href }) {
+export function FooterAuth({ text1, text2, onClick }) {
 	return (
-		<p className="footer-auth">{text1} <a href={href}>{text2}</a></p>
+		<p className="footer-auth">{text1} <button onClick={onClick}>{text2}</button></p>
 	);
 }
