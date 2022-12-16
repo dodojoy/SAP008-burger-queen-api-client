8cb9345b04a8c351ff97e2c6bfbf5ced23788c58
joyce <joyce.mbt@gmail.com>
2022-12-16 17:35:16 -0300

    tratando os erros através do useState ao invés de utilizar queryselector

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -13,21 +13,21 @@ import { saveUser } from "../../contexts/api";
 export const Login = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [handleError, setHandleError] = useState('');
 	const navigate = useNavigate();
 	const error = errorMessage.error;
 
 	const loginEvent = (e) => {
 		e.preventDefault();
-		const tagErrorMessage = document.querySelector('#error-message');
 		if (email === '' || password === '') {
-			tagErrorMessage.innerHTML = 'Preencha todos os campos'
+			setHandleError('Preencha todos os campos')
 		} else {
 			login(email, password)
 				.then((response) => {
 					if (response.status === 200) {
 						return response.json();
 					}
-					tagErrorMessage.innerHTML = error[0].login[response.status];
+					setHandleError(error[0].login[response.status]);
 				})
 				.then((data) => {
 					if (!data) return;
@@ -39,7 +39,7 @@ export const Login = () => {
 					saveToken(data.token);
 					saveUser(data.name);
 				})
-				.catch((erro) => console.log(erro));
+				.catch((error) => setHandleError(error));
 		};
 	};
 
@@ -50,7 +50,7 @@ export const Login = () => {
 				<form>
 					<Inputs type='email' placeholder='E-MAIL' onChange={(e) => setEmail(e.target.value)} class /><br />
 					<Inputs type='password' placeholder='SENHA' onChange={(e) => setPassword(e.target.value)} /><br />
-					<p id='error-message'></p>
+					<p id='error-message'>{handleError}</p>
 					<Inputs type='submit' value='ENTRAR' onClick={loginEvent}/>
 				</form>
 				<FooterAuth text1='Não possui uma conta?' text2='Cadastre-se!' onClick={() => navigate('/register')} />
