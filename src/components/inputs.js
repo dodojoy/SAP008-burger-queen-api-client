119ce73f2b7175fe666202ef5a331ed1d50efdc1
joyce <joyce.mbt@gmail.com>
2022-12-19 23:11:02 -0300

    fiz uma alteração do componente input para cada um ter uma classlist especificada na chamada do componente

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -1,6 +1,6 @@
-export function Inputs({ value, type, placeholder, onClick, onChange }) {
+export function Inputs({ value, type, placeholder, onClick, onChange, className, id }) {
 	return (
-		<input type={type} className="input-auth" placeholder={placeholder} value={value} 
+		<input id={id} type={type} className={className} placeholder={placeholder} value={value} 
 		onClick={onClick} onChange={onChange} />
 	);
 }
