b3f66ddd691ef8f627ea767ac2bc1070aea3e2f3
joyce <joyce.mbt@gmail.com>
2022-12-20 23:16:50 -0300

    mudei de aspas simples para duplas

--- src/components/orderCard.js
+++ src/components/orderCard.js
@@ -1,18 +1,18 @@
-import { Inputs } from './inputs';
+import { Inputs } from "./inputs";
 import { AiOutlinePlus, AiOutlineLine } from "react-icons/ai";
 
 
-export function Order({ id, number, text, client, tableNumber, numberOrder, products, total, handleOnClick }) {
+export function Order({ number, text, client, tableNumber, numberOrder, products, total, handleOnClick }) {
 	return (
         <section className="order-content">
             <div className="order-header">
                 <div> 
-                    <label for='nome-do-cliente'>Cliente: </label>
-                    <Inputs type={text} id={'nome-do-cliente'}>{client}</Inputs>
+                    <label for="nome-do-cliente">Cliente: </label>
+                    <Inputs type={text} id={"nome-do-cliente"}>{client}</Inputs>
                 </div>
                 <div> 
-                    <label for='mesa-do-cliente'>Mesa: </label>
-                    <Inputs type={number} id={'mesa-do-cliente'}>{tableNumber}</Inputs>
+                    <label for="mesa-do-cliente">Mesa: </label>
+                    <Inputs type={number} id={"mesa-do-cliente"}>{tableNumber}</Inputs>
                 </div>
                 <p>Pedido: {numberOrder}</p>
             </div>
@@ -25,11 +25,11 @@ export function Order({ id, number, text, client, tableNumber, numberOrder, prod
 
 export function Items({ name, price, handleOnClick }) {
 	return (
-        <div className="item-content">
+        <div className="item-content comanda">
             <p className="product">{name}</p>
             <p className="price">R$ {price}</p>
-            <AiOutlinePlus className='plus-sign' onClick={handleOnClick}/>
-            <AiOutlineLine className='plus-sign'></AiOutlineLine>
+            <AiOutlinePlus className="plus-sign" onClick={handleOnClick}/>
+            <AiOutlineLine className="plus-sign"/>
         </div>
 	);
 }
