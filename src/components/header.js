b2a4f485a2cece41283dbf3008a3f009b4d34d7b
joyce <joyce.mbt@gmail.com>
2023-01-12 14:05:39 -0300

    coloquei as imagens da logo e do logout diretamente no componente

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
-export function HeaderSalon({ atendente, logo, logout }) {
+import logo from '../assets/logo.svg';
+import logout from '../assets/logout.svg';
+
+export function HeaderSalon({ role, nameRole, handleOnClick }) {
 	return (
 		<div className='header-salon'>
             <img className='logo' src={logo} alt='logo'></img>
-            <p className="role-name">Atendente: {atendente}</p>
-            <img className='logout' src={logout} alt='logout logo'></img>
+            <p className="role-name">{role}: {nameRole}</p>
+            <img className='logout' src={logout} alt='logout logo' onClick={handleOnClick}></img>
         </div>
 	);
 }
