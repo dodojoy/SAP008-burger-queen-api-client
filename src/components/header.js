0dfcc5e9f0281cc3ee9b73ab28115aad5ebe4996
joyce <joyce.mbt@gmail.com>
2023-01-19 22:54:35 -0300

    fiz uma mudança de tag, de div para header

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,10 @@ import logout from '../assets/logout.svg';
 
 export function HeaderSalon({ role, nameRole, handleOnClick }) {
 	return (
-		<div className='header-salon'>
+		<header className='header-salon'>
             <img className='logo' src={logo} alt='logo'></img>
             <p className="role-name">{role}: {nameRole}</p>
             <img className='logout' src={logout} alt='logout logo' onClick={handleOnClick}></img>
-        </div>
+        </header>
 	);
 }
