afdcab4f30e12a2229b5dd2eb979c60e3390d76c
joyce <joyce.mbt@gmail.com>
2023-01-20 21:59:34 -0300

    adicionei uma div container

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,11 @@ import logout from '../assets/logout.svg';
 export function HeaderSalon({ role, nameRole, handleOnClick }) {
 	return (
 		<header className='header-salon'>
-            <img className='logo' src={logo} alt='logo'></img>
-            <p className="role-name">{role}: {nameRole}</p>
-            <img className='logout' src={logout} alt='logout logo' onClick={handleOnClick}></img>
+            <div className='header-container'>
+                <img className='logo' src={logo} alt='logo'></img>
+                <p className="role-name">{role}: {nameRole}</p>
+                <img className='logout' src={logout} alt='logout logo' onClick={handleOnClick}></img>
+            </div>
         </header>
 	);
 }
